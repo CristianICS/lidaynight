a6519bd0ec6f9ed6662fbae582145782e4f52440
Cristian Iranzo <iranzocristian@gmail.com>
2026-09-01 12:11:25 +0200

    Fix typo

--- R/plot_stats.R
+++ R/plot_stats.R
@@ -396,8 +396,8 @@ make_violin_plot <- function(stats_long) {
   "Points with only single returns (%)" = "ret_single_pct",
   "Points with more than one return (%)" = "ret_abvone_pct",
   "Points with negative heights (%)" = "pnts_negative_height_pct",
-  "Outliers below height 1st quantile" = "pnts_height_p01",
-  "Outliers above height 99th quantile" = "pnts_height_p99"
+  "Outliers below height 1st quantile" = "pnts_height_lt_q01",
+  "Outliers above height 99th quantile" = "pnts_height_gt_q99"
 )
 
 .target_stats <- c(
